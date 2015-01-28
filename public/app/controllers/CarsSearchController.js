0e4460d67720bcab9cf4c986965feb243415a2eb
vdtodorov93 <vdtodorov93@gmail.com>
2015-01-29 01:16:08 +0200

    The result grid is now sortable

--- public/app/controllers/CarsSearchController.js
+++ public/app/controllers/CarsSearchController.js
@@ -1,20 +1,10 @@
-app.controller('CarsSearchController', function($scope, $location, cachedBrandsCatalogue, ModelsResource, CarAdsService, $timeout) {
+app.controller('CarsSearchController', function($scope, $location, cachedBrandsCatalogue, ModelsResource, CarAdsService) {
   $scope.brands = cachedBrandsCatalogue.query();
   $scope.options = {};
-//  $scope.results = [
-//    {
-//      brand: {
-//        name: 'bmw'
-//      },
-//      model: {
-//        name: '300'
-//      },
-//      fuelType: 'Gas',
-//      gearBox: 'Manual',
-//      price: 200,
-//      year: 2008
-//    }];
-  //$scope.options.selectedBrand = $scope.brands[0];
+  $scope.sortBy = 'year';
+  $scope.sortPrefix = '';
+  var sortAsc = true;
+
   $scope.changeBrand = function() {
     if($scope.options.selectedBrand) {
       $scope.models = ModelsResource.query({ brandId: $scope.options.selectedBrand });
@@ -27,20 +17,26 @@ app.controller('CarsSearchController', function($scope, $location, cachedBrandsC
   $scope.search = function(options) {
     console.dir(options);
     CarAdsService.searchByOptions(options)
-      .then(function(carsP) {
-        $scope.cars = carsP.cars;
-        //angular.copy($scope.cars, cars);
-        //$location.path('/searchResults');
-        //$timeout(function() {
-//          $scope.cars2 = cars;
-//          console.log($scope.cars2);
-//          angular.copy($scope.cars2, $scope.cars);
-//          console.log($scope.cars);
-
-        //})
-
+      .then(function(response) {
+        $scope.cars = response.cars;
       }, function(err) {
         console.log(err);
       })
+  };
+
+  $scope.changeSort = function(newSort) {
+    if(newSort === $scope.sortBy) {
+      if(sortAsc) {
+        $scope.sortPrefix = '-'
+      } else {
+        $scope.sortPrefix = '';
+      }
+
+      sortAsc = !sortAsc;
+    } else {
+      $scope.sortBy = newSort;
+      $scope.sortPrefix = '';
+      sortAsc = true;
+    }
   }
 });
